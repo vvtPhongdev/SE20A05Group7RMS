a5ed375e10752aaae93b871f60bdfbf289cf3139
LyHD_DE190075 <lichsu86nbk@gmail.com>
2026-06-02 01:55:31 +0700

    T-034 Implement plan-locked enforcement middleware

--- services/recruiting/src/recruiting.module.ts
+++ services/recruiting/src/recruiting.module.ts
@@ -5,8 +5,9 @@ import { ApplicationsModule } from './modules/applications/applications.module';
 import { InvitesModule } from './modules/invites/invites.module';
 import { EvaluationsModule } from './modules/evaluations/evaluations.module';
 import { TalentSearchModule } from './modules/talent-search/talent-search.module';
+import { PlanGuardModule } from './common/plan-guard/plan-guard.module';
 
 @Module({
-  imports: [DatabaseModule, RolesModule, ApplicationsModule, InvitesModule, EvaluationsModule, TalentSearchModule],
+  imports: [DatabaseModule, RolesModule, ApplicationsModule, InvitesModule, EvaluationsModule, TalentSearchModule, PlanGuardModule],
 })
 export class RecruitingModule {}
