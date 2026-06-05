38a27325053976a4e3a1c3095e21068c3a8261df
dungnt585 <ngotuandung585@gmail.com>
2026-06-05 12:10:05 +0700

    fix T039

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -5,6 +5,7 @@ import { Layout } from './components/Layout';
 import { Login } from './pages/Login';
 import { LandingPage } from './pages/LandingPage';
 import { SignUp } from './pages/SignUp';
+import { ForgotPassword } from './pages/ForgotPassword';
 import { Unauthorized } from './pages/Unauthorized';
 import { AdminDashboard } from './pages/AdminDashboard';
 import { DeptHeadDashboard } from './pages/DeptHeadDashboard';
@@ -42,6 +43,7 @@ export function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/unauthorized" element={<Unauthorized />} />
 
           {/* Protected routes wrapped inside the app shell Layout */}
