5762ae52695998863fb42323402d0c753c2deba4
duyhieu141 <nlbtboss1@gmail.com>
2026-07-27 21:22:36 +0700

    fix: use prisma accelerate client

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 type PrismaLogLevel = 'query' | 'warn' | 'error';
@@ -41,5 +41,5 @@ if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
 }
 
-export * from '@prisma/client/edge';
+export * from '@prisma/client';
 export * from './audit-log.service';
